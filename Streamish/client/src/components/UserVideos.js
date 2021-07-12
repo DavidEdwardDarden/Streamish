120d9995890dc34b8e98693bb1ca4239e3d39f6e
DavidEdwardDarden <davidedarden83@gmail.com>
2021-07-12 14:32:05 -0500

    search function added I had to go back to the last chapter so it doesn't line up with my branch name

---
+++ Streamish/client/src/components/UserVideos.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import { Card, CardBody } from "reactstrap";
+// import { Link } from "react-router-dom";
+
+// export const UserVideos = ({ video }) => {
+//   return (
+    
+//   );
+// };
+
--- Streamish/client/src/components/Video.js
+++ Streamish/client/src/components/Video.js
@@ -1,34 +1,43 @@
 import React from "react";
 import { Card, CardBody } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Video = ({ video }) => {
   return (
-    <Card >
+    <Card>
       <p className="text-left px-2">Posted by: {video.userProfile.name}</p>
       <CardBody>
-        <iframe className="video"
-          src={video.url}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen />
+        <div>
+          <iframe
+            className="video"
+            src={video.url}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+
+        <Link to={`/videos/${video.id}`}>
+          <strong>{video.title}</strong>
+        </Link>
 
         <p>
           <strong>{video.title}</strong>
         </p>
-       
+
         <p>{video.description}</p>
 
-        <p>{video.comments?.map(comment => {
-            return <li>{comment.message}</li>
-        })}</p>
-        
+        <p>
+          {video.comments?.map((comment) => {
+            return <li>{comment.message}</li>;
+          })}
+        </p>
+
         {/* <p>{video.comments.map((comment) => (
                     <p>{comment.userProfileId}: {comment.message}</p>
                 ))}
                 </p> */}
-
-
       </CardBody>
     </Card>
   );
